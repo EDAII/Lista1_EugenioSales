eee47e7dc6df03a89ad9ce45e486dcb7a40adb42
Eugenio Sales <eugenio.sales@outlook.com>
2019-08-30 10:54:25 -0300

    Update communication between server-client

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
-from flask import Flask, render_template, url_for, request, jsonify, redirect
+from flask import Flask, render_template, url_for, request, jsonify, redirect, json
 from methods.linear_search import linear_search
 from random import sample
 
 app = Flask(__name__)
 
+def parameters(method):
+    if method == 'Linear':
+        array = [1, 2, 3, 4, 7, 8]
+        counter = linear_search(array, 7)
+        print(counter)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+
+    value = [12, 1, 11, 1, 2, 4]
+
     if request.method == 'POST':
-        firstSeek  = request.form['Submit']
-        print(firstSeek)
-    return render_template("index.html")
+        forms = {
+            'first': request.form['primeiraBusca'], 
+            'second': request.form['segundaBusca']
+        }
 
-@app.route('/data', methods=['POST', 'GET'])
-def data():
-    data = {
-        'first': request.form['primeiraBusca'], 
-        'second': request.form['segundaBusca']
-    }
-    print(data['first'])
+        print(forms['first'])
+        searchMeth = forms['first']
 
-    return render_template("index.html", data=data)
-    #return redirect(url_for('index'))
+        return render_template("index.html", value=json.dumps(value), searchMeth=searchMeth)
+    
+    return render_template("index.html", value=json.dumps(value))
+
+#ed2
 
 @app.route('/ajax', methods=['POST', 'GET'])
 def ajax():
     return jsonify({'results' : sample(range(1, 10), 5)})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
